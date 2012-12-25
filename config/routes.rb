fd38ee4e164757dabc8798a8189138203d53ee02
Thomas Chen <foxnewsnetwork@gmail.com>
2012-12-25 14:29:54 -0800

    stagged some more stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Dance::Application.routes.draw do
+  devise_for :users
+
+  resources :users, :only => [:show, :destroy, :edit, :update, :index]
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
