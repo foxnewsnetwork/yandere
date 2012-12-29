f9e07e2781a2f58087c5a712851ac28839b1e61d
Thomas Chen <foxnewsnetwork@gmail.com>
2012-12-28 19:56:40 -0800

    defalt staging for meeting domin

---
+++ app/domain/meeting.rb
@@ -0,0 +1,6 @@
+
+# Gem style path includes and whatnot.
+# We expose all the external apis in this file
+class Meeting
+	# Put loading junk in here
+end # Meeting
--- spec/domain/meeting_spec.rb
+++ spec/domain/meeting_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Meeting do 
-
+	describe "Sanity Test" do 
+		it "should not be null" do 
+			Meeting.should_not be_nil
+		end # it
+	end # Sanity
 end # Meeting
